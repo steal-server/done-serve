f7dec968fa356bbe784cf7d881a2cf9fd245e020
Matthew Phillips <matthew@matthewphillips.info>
2016-03-08 17:10:38 -0500

    Make work in CI

--- test/test.js
+++ test/test.js
@@ -1,28 +1,11 @@
-var spawn = require("cross-spawn-async");
-var path = require("path");
-var mochaPath = path.join(path.dirname(require.resolve("mocha")),
-						  "bin", "mocha");
+var mochas = require("spawn-mochas");
 
 var tests = [
 	"server_test.js",
 	"cookie_server_test.js",
 	"can-serve_test.js"
-];
+].map(function(pth){
+	return __dirname + "/" + pth;
+});
 
-runTests(tests);
-
-function runTests(tests) {
-	var pth = path.join(__dirname, tests.shift());
-	var child = spawn("node", [mochaPath, pth], {
-		stdio: "inherit"
-	});
-	child.on("exit", function(code){
-		if(code !== 0) {
-			process.exit(code);
-		} else if(tests.length) {
-			runTests(tests);
-		} else {
-			process.exit(code);
-		}
-	});
-}
+mochas(tests);
