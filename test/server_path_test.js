d9404b6c00443394f3cc2b1431ee022c9d899837
Matthew Phillips <matthew@matthewphillips.info>
2016-04-22 10:21:28 -0400

    Breaking test for #5
    This adds a breaking test for #5, demonstrating that socket.io proxying
    doesn't work when the proxy destination in a subpath like
    `http://localhost:6060/api`

---
+++ test/server_path_test.js
@@ -0,0 +1,75 @@
+var assert = require('assert');
+var path = require('path');
+var http = require('http');
+var request = require('request');
+var socketio = require('socket.io');
+var socketClient = require('socket.io-client');
+
+var serve = require('../lib/index');
+
+describe('done-serve tests', function() {
+	this.timeout(10000);
+
+	var server, other, io;
+
+	before(function(done) {
+		server = serve({
+			path: path.join(__dirname, 'tests'),
+			proxy: 'http://localhost:6060/api',
+		}).listen(5050);
+
+		other = http.createServer(function(req, res) {
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.end('Other server\n');
+		}).listen(6060);
+
+		io = socketio().listen(other);
+
+		server.on('listening', done);
+	});
+
+	after(function(done) {
+		server.close(done);
+	});
+
+	it('proxies to other servers on a path', function(done) {
+		request('http://localhost:5050/api/', function(err, res, body) {
+			assert.equal(body, 'Other server\n', 'Got message from other server');
+			done();
+		});
+	});
+
+	describe('proxying socket.io websockets', function(){
+		beforeEach(function(){
+			this.oldDocument = global.document;
+			delete global.document;
+		});
+
+		afterEach(function(){
+			global.document = this.document;
+		});
+
+		it('proxies Socket.io websockets', function(done) {
+			var original = { hello: 'world' };
+
+			io.on('connection', function (socket) {
+			  socket.emit('news', original);
+			});
+
+			var socket = socketClient('http://localhost:5050');
+
+			socket.on('news', function(data) {
+				assert.deepEqual(data, original);
+				socket.disconnect();
+			});
+
+			socket.on('disconnect', function() {
+				// Timeout for Socket.io cleanup on slower machiens (like CI) finish
+				setTimeout(function() {
+					done();
+				}, 500);
+			});
+		});
+
+	});
+});
