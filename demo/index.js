000ecec3b1cc5531236b06a93d01f27852850d51
Matthew Phillips <matthew@matthewphillips.info>
2017-02-10 16:40:58 -0500

    Push out the demo

--- demo/index.js
+++ demo/index.js
@@ -1,6 +1,11 @@
+var other = require("./other");
 
 module.exports = function(){
 	var div = document.createElement("div");
 	div.textContent = "Hello world";
 	document.body.appendChild(div);
+
+	var script = document.createElement("script");
+	script.setAttribute("src", "./node_modules/steal/steal.js");
+	document.body.appendChild(script);
 };
